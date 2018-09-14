4b6ddb6a9d41e3cb855bd518b16e8b2383b9f016
Zacheriah Mell <zachm93@live.com>
2018-09-14 11:27:34 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -12,7 +12,7 @@ request = portal.context.makeRequestRSpec()
 
 node1 = request.XenVM("node-1")
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-# node1.routable_control_ip = True
+node1.routable_control_ip = True
 
 iface1 = node1.addInterface("if1")
 iface1.component_id = "eth1"
@@ -25,22 +25,25 @@ iface2 = node2.addInterface("if2")
 iface2.component_id = "eth2"
 iface2.addAddress(pg.IPv4Address("192.168.1.2",  "255.255.255.0"))
 
-# add links
+node3 = request.XenVM("node-3")
+node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+iface3 = node3.addInterface("if3")
+iface3.component_id = "eth3"
+iface3.addAddress(pg.IPv4Address("192.168.1.3",  "255.255.255.0"))
+
+
+node4 = request.XenVM("node-4")
+node4.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+iface4 = node4.addInterface("if4")
+iface4.component_id = "eth4"
+iface4.addAddress(pg.IPv4Address("192.168.1.4",  "255.255.255.0"))
+
+# add network links
 link = request.LAN("lan")
 link.addInterface(iface1)
 link.addInterface(iface2)
+link.addInterface(iface3)
+link.addInterface(iface4)
 
-# node3 = request.XenVM("node-3")
-# node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-# iface3 = node3.addInterface("if3")
-# iface3.component_id = "eth3"
-# iface3.addAddress(pg.IPv4Address("192.168.1.3",  "255.255.255.0"))
-
-
-# node4 = request.XenVM("node-4")
-# node4.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-# iface4 = node4.addInterface("if4")
-# iface4.component_id = "eth4"
-# iface4.addAddress(pg.IPv4Address("192.168.1.4",  "255.255.255.0"))
 
 portal.context.printRequestRSpec()

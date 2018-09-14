5bbfb096dcd19f444b954dc8d7c1825403b4e640
Zacheriah Mell <zachm93@live.com>
2018-09-13 22:04:43 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,26 +1,31 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
-
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
-
-Instructions:
-Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
-"""
-
 import geni.portal as portal
 import geni.rspec.pg as pg
 
 request = portal.context.makeRequestRSpec()
 
-for i in range(1, 5):
- node = request.XenVM("node-%d" % i)
- 
 
- node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node1 = request.XenVM("node-1")
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+iface1 = node1.addInterface("if1")
+iface1.addAddress(pg.IPv4Address("192.168.1.1"  "255.255.255.0"))
+node1.routable_control_ip = True
 
- iface = node.addInterface("if1")
- iface.addAddress(pg.IPv4Address("192.168.1.%d" % i, "255.255.255.0"))
 
- if (i == 1):
-  node.routable_control_ip = True
+node2 = request.XenVM("node-2")
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+iface2 = node2.addInterface("if1")
+iface2.addAddress(pg.IPv4Address("192.168.1.2"  "255.255.255.0"))
 
- portal.context.printRequestRSpec()
+
+node3 = request.XenVM("node-3")
+node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+iface3 = node3.addInterface("if1")
+iface3.addAddress(pg.IPv4Address("192.168.1.3"  "255.255.255.0"))
+
+
+node4 = request.XenVM("node-4")
+node4.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+iface4 = node4.addInterface("if1")
+iface4.addAddress(pg.IPv4Address("192.168.1.4"  "255.255.255.0"))
+
+portal.context.printRequestRSpec()
